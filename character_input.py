69aeb82c657a7293dddb207e7301e96fd4013529
therealheadache <69644469+therealheadache@users.noreply.github.com>
2020-08-21 15:15:07 -0700

    Create character_input.py

---
+++ character_input.py
@@ -0,0 +1,5 @@
+name = input('Please type in your name: ')
+age = int(input('Please tell us your age: '))
+
+print('Thank you ' + (name), 'and you are ', (age))
+print('You will be 100 years old in the year: ', (2020 + (100 - (age))))
